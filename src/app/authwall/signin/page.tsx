b96c44726847e7b0d5d93bf2ad8b378e5e284299
Tanvir Hossain <developer.tanvirhossain@gmail.com>
2024-12-10 10:26:44 +0600

    feat: 🎸 Project initialized with shadcn and dasboard done
    ✅ Closes: zero issues done by this commit

---
+++ src/app/authwall/signin/page.tsx
@@ -0,0 +1,9 @@
+import { SigninForm } from "@/pages/auth/signin/signin-form";
+
+export default function Page() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center px-4">
+      <SigninForm />
+    </div>
+  );
+}
